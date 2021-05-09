2dd4978b1260cf8e298e48d701c2958e6d798246
tartie sorel <sorelaugustin@gmail.com>
2021-05-09 21:56:55 +0100

    set up new game

--- MagicSquare/MagicSquare.Shared/MainPage.xaml.cs
+++ MagicSquare/MagicSquare.Shared/MainPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +13,51 @@ namespace MagicSquare
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private int[] numbers;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            foreach (var item in container.Children.Cast<Button>())
+                item.Click += new RoutedEventHandler(Buttons_Click);
+
+            SetUpNewGame();
+        }
+
+        private void SetUpNewGame()
+        {
+            // SetUp array
+            numbers = new int[9];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (true)
+                {
+                    int x = random.Next(0, 10);
+
+                    if (!numbers.Contains(x))
+                    {
+                        numbers[i] = x;
+                        break;
+                    }
+                }
+            }
+
+            // Display array
+            int index = -1;
+            foreach (var item in container.Children.Cast<Button>())
+            {
+                index++;
+                item.Content = numbers[index] == 9 ? string.Empty : (object)numbers[index];
+            }
+
+        }
+
+        private void Buttons_Click(object sender, RoutedEventArgs e)
+        {
+            SetUpNewGame();
         }
     }
 }
